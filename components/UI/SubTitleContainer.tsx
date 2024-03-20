7128200d88b2708558d0c5cca908eee5ba796db1
Anthony Legrain <legrain.anthony51@gmail.com>
2024-03-21 00:20:26 +0100

    responsive univers

--- components/UI/SubTitleContainer.tsx
+++ components/UI/SubTitleContainer.tsx
@@ -1,6 +1,6 @@
-import { FontStyle } from "@/styles";
+import { FontStyle, HomeStyle } from "@/styles";
 import { UiComponentProps } from "@/types";
 
 export const SubTitleContainer: React.FC<UiComponentProps> = ({ children, className = '' }) => {
-  return <div className={`${className} ${FontStyle.manthone_sb}`}>{children}</div>;
+  return <div className={`${className} ${FontStyle.manthone_sb} ${HomeStyle.subtitle__container}`}>{children}</div>;
 };
